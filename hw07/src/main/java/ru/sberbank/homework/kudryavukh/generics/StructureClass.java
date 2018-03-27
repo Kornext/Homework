cc3d810c9cbe89e731757fcfe330ec053e9ae8eb
Kornext <kornext@gmail.com>
2018-03-27 03:21:25 +0300

    HW 7 repair

--- hw07/src/main/java/ru/sberbank/homework/kudryavukh/generics/StructureClass.java
+++ hw07/src/main/java/ru/sberbank/homework/kudryavukh/generics/StructureClass.java
@@ -5,46 +5,28 @@ import ru.sberbank.homework.kudryavukh.generics.CountMap;
 import java.util.*;
 
 public class StructureClass<T> implements CountMap<T> {
-    private Map<Integer, T> map = new HashMap<>();
+    private Map<T, Integer> map = new HashMap<>();
     private int keyMap = 0;
 
 
     @Override
     public void add(T type) {
-        map.put(keyMap, type);
-        keyMap++;
+        Integer n = map.get(type);
+        if (n != null) {
+            map.put(type, n++);
+        } else {
+            map.put(type, 1);
+        }
     }
 
     @Override
     public int getCount(T type) {
-        int count = 0;
-        for (Map.Entry<Integer, T> e : map.entrySet()) {
-            T value = e.getValue();
-
-            if (value.equals(type)) {
-                count++;
-            }
-        }
-        return count;
+        return map.getOrDefault(type, 0);
     }
 
     @Override
     public int remove(T type) {
-        int countRemoveValue = getCount(type);
-        if (countRemoveValue == 0) {
-            return countRemoveValue;
-        }
-        Set<Map.Entry<Integer, T>> entrySet = map.entrySet();
-        List<Integer> keyValue = new ArrayList<>();
-        for (Map.Entry<Integer, T> pair : entrySet) {
-            if (type.equals(pair.getValue())) {
-                keyValue.add(pair.getKey());
-            }
-        }
-        for (int key : keyValue) {
-            map.remove(key);
-        }
-        return countRemoveValue;
+        return map.remove(type);
     }
 
     @Override
@@ -52,55 +34,38 @@ public class StructureClass<T> implements CountMap<T> {
         return map.size();
     }
 
-    /**
-     * Метод собирает в два листа ключи и значения добавляемой мапы, после чего
-     * ищет совпадение по ключу в текущей мапе. Если находит - умножает ключ на 2
-     * и опять сверяется на возможность совпадения. Если ключ 0 - преобразует его
-     * в еденицу.
-     *
-     * @param source
-     */
     @Override
     public void addAll(CountMap<T> source) {
-        Set<Map.Entry<Integer, T>> entrySet = map.entrySet();
-        List<Integer> keysSource = new ArrayList<>();
-        List<T> valuesSource = new ArrayList<>();
+        Set<Map.Entry<T, Integer>> entrySet = source.toMap().entrySet();
+        List<T> keysSource = new ArrayList<>();
+        List<Integer> valuesSource = new ArrayList<>();
 
-        for (Map.Entry<Integer, T> pair : entrySet) {
+        for (Map.Entry<T, Integer> pair : entrySet) {
             keysSource.add(pair.getKey());
             valuesSource.add(pair.getValue());
         }
+
         for (int i = 0; i < keysSource.size(); i++) {
-            int key = keysSource.get(i);
-            T value = valuesSource.get(i);
-            if (map.get(key) != null && key == 0) {
-                key++;
+            T key = keysSource.get(i);
+            Integer value = map.get(key);
+            if (value != null) {
+                map.put(key, value + valuesSource.get(i));
+            } else {
+                map.put(key, valuesSource.get(i));
             }
-            while (map.get(key) != null) {
-                key += key;
-            }
-            map.put(key, value);
         }
-        //map.putAll((Map<? extends Integer, ? extends T>) source);
     }
 
     @Override
-    public Map toMap() {
+    public Map<T, Integer> toMap() {
         Map<T, Integer> resultMap = new HashMap<>();
-
-        for (Map.Entry<Integer, T> e : map.entrySet()) {
-            T value = e.getValue();
-            if (!resultMap.containsValue(value)) {
-                int countValue = getCount(value);
-                resultMap.put(value, countValue);
-            }
-        }
-
+        toMap(resultMap);
         return resultMap;
     }
 
     @Override
-    public void toMap(Map destination) {
-        destination = toMap();
+    public void toMap(Map<T, Integer> destination) {
+
+        destination.putAll(map);
     }
 }
